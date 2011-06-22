57a00d3f288f15448f3ae079114e1b468ed9ebcd
Ferdi <efcm@vdwerf.eu>
2011-06-22 10:09:47 +0200

    preg_replace requires 3 arguments: pattern, replacement, subject.

--- inc/modules/users/users.class.php
+++ inc/modules/users/users.class.php
@@ -724,7 +724,7 @@ class users extends baseModule{
                 $eSubject = $isSelfActivation ? $pConf['email_subject'] : $pConf['emailadmin_subject'];
                 $eFrom = $isSelfActivation ? $pConf['email_from'] : $pConf['emailadmin_from'];
                 $eTemplate = $isSelfActivation ? $pConf['email_template'] : $pConf['emailadmin_template'];
-                $sendTo = preg_replace( "/[\r\n]/", $values['email']);
+                $sendTo = preg_replace( "/[\r\n]/", "", $values['email']);
                 $actPage = $isSelfActivation ? $pConf['email_actpage'] : $pConf['emailadmin_actpage'];
                 
                 tAssign('values', $values);
