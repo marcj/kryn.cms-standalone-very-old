52bc2c6ed9567071c3a5c29fc2ec3f1285308f22
MArc <marc@kryn.org>
2011-11-29 19:42:13 +0100

    wrong array check function was used, which proceeded ugly warning in the error log

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if( !file_exists('inc/config.php') ){
 };
 include('inc/config.php');
 
-$umask = (in_array('umask', $cfg))?$cfg['umask']:002;
+$umask = (array_key_exists('umask', $cfg))?$cfg['umask']:002;
 @umask($umask);
 
 if( !array_key_exists('display_errors', $cfg) )
