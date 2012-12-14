15800222bd48217a77cf601ba3fd8c9820ddaf35
MArcJ <marc@kryn.org>
2012-12-14 11:51:03 +0100

    Fixed invalid call

--- lib/smarty/plugins/function.page.php
+++ lib/smarty/plugins/function.page.php
@@ -6,7 +6,7 @@ function smarty_function_page( $params, &$smarty ){
     $params['withRessources'] = $params['withRessources'] ? true : false;
     
     if( $params['id']+0 > 0 ){
-        return Core\Render::renderPageContents( $params['id'], $params['slot'], $params );
+        return Core\Render::renderContents( $params['id'], $params['slot'], $params );
     } else
         return '';
         
