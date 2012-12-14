8059e740e961c8b1aab338884c5d67425f3dbe8b
MArcJ <marc@kryn.org>
2012-12-14 11:19:11 +0100

    Bash typo and add files for previous commit

---
+++ core/model/AppLock.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core;
+
+use Core\om\BaseAppLock;
+
+
+/**
+ * Skeleton subclass for representing a row from the 'kryn_system_app_lock' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.kryn
+ */
+class AppLock extends BaseAppLock {
+
+} // AppLock
