be0e6974fb14f7403ce4e5729324171477901413
MArcJ <marc@kryn.org>
2012-12-15 00:07:57 +0100

    Outsourced basic apache setup in travis.yml

---
+++ test/script/setup-apache-fcgi.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PHP=`which php`;
+
+# install stuff
+sudo apt-get install apache2 libapache2-mod-fcgid;
+sudo a2enmod actions;
+sudo a2enmod rewrite;
+sudo a2enmod fcgid;
+sudo a2enmod suexec;
+
+#setup vhost+fcgi config
+cat test/config/default.apache2-vhost.conf | sed -e "s,ROOTPATH,`pwd`,g" | sudo tee /etc/apache2/sites-available/default > /dev/null;
+
+# create fgi script
+echo -e "#!/bin/sh\n$PHP" | sudo tee ../php-fcgi > /dev/null;
+
+# restart apache
+sudo service apache2 restart;
