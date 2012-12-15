206145768a6011e1367c50713fc4b144ff0d7c89
MArcJ <marc@kryn.org>
2012-12-15 05:36:08 +0100

    PHPunit should now be started from the root. anything adjusted that all tests works.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -5,7 +5,7 @@
 spl_autoload_register(function ($pClass) {
 
     $pClass = substr($pClass, strpos($pClass, '\\')+1);
-    $fullClazz = 'Tests/'.str_replace('\\', '/', $pClass).'.class.php';
+    $fullClazz = 'test/Tests/'.str_replace('\\', '/', $pClass).'.class.php';
 
     if (file_exists($fullClazz)){
         include $fullClazz;
@@ -18,6 +18,6 @@ include_once(dirname(__FILE__).'/../core/global/exceptions.global.php');
 error_reporting(E_COMPILE_ERROR|E_RECOVERABLE_ERROR|E_ERROR|E_CORE_ERROR|E_USER_ERROR);
 ini_set('display_errors', 1);
 
-if (file_exists('../config.php')){
+if (file_exists(dirname(__FILE__).'/../config.php')){
     die("It seems Kryn.cms is already installed! Please delete the config.php. aborted.\n");
 }
