736429b6796474cafb1eb4f9a75f84e166d75cc1
MArcJ <marc@kryn.org>
2013-01-14 03:05:43 +0100

    Added propel model classes for Core\\Workspace, Test\\*

---
+++ module/test/model/ItemVersionPeer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Test;
+
+use Test\om\BaseItemVersionPeer;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'kryn_test_item_version' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.Test
+ */
+class ItemVersionPeer extends BaseItemVersionPeer
+{
+}
