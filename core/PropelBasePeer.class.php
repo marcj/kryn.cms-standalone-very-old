7368761ea07c69222ffce77398afbed10c576ef7
MArcJ <marc@kryn.org>
2013-02-12 03:45:08 +0100

    Invalidate the *PropelCacheObject cache if a propel objects is updated

--- core/PropelBasePeer.class.php
+++ core/PropelBasePeer.class.php
@@ -14,6 +14,26 @@ class PropelBasePeer extends \BasePeer {
         static::$ignoreNextDoSelect = $pValue;
     }
 
+    public static function doUpdate(\Criteria $selectCriteria, \Criteria $updateValues, \PropelPDO $con) {
+        //remove propel cache
+        $dbMap = \Propel::getDatabaseMap($selectCriteria->getDbName());
+
+        $tableName = $selectCriteria->getPrimaryTableName();
+
+        if (!$tableName){
+            $keys = $selectCriteria->keys();
+            if (!empty($keys))
+                $tableName = $selectCriteria->getTableName( $keys[0] );
+        }
+        if ($tableName){
+            $table = $dbMap->getTable($tableName);
+            $peer = $table->getPeerClassname();
+            Kryn::removePropelCacheObject($peer::OM_CLASS);
+        }
+        return parent::doUpdate($selectCriteria, $updateValues, $con);
+    }
+
+
     public static function doSelect(\Criteria $criteria, \PropelPDO $con = null){
 
         if (self::getIgnoreNextDoSelect()){
