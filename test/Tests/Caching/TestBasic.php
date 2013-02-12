ef95d2cd8223df4f56dec5bb3e2f8e261263ab79
MArcJ <marc@kryn.org>
2013-02-12 03:47:03 +0100

    Added a test that proves that cache invalidation works.

---
+++ test/Tests/Caching/TestBasic.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Caching;
+
+use Tests\TestCaseWithCore;
+
+use Core\Kryn;
+
+class BasicTest extends TestCaseWithCore {
+
+
+    public function testGeneral(){
+
+
+        //invalidation check
+        $this->assertTrue(Kryn::setCache('core/test/2', 'Test Object number 2'));
+
+        $this->assertTrue(Kryn::invalidateCache('core/test'));
+        usleep(1000*50); //50ms
+
+        $this->assertFalse(Kryn::getCache('core/test/2'));
+
+
+        //without invalidation
+        $this->assertTrue(Kryn::setCache('core/test/2', 'Test Object number 2'));
+        $this->assertEquals('Test Object number 2', Kryn::getCache('core/test/2'));
+
+
+    }
+
+}
