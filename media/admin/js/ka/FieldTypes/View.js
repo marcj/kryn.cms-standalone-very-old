b81397f613b24d0783e6487d4a41dc3cbd837791
MArcJ <marc@kryn.org>
2013-02-13 22:42:53 +0100

    Implemented ka.FieldTypes.view completely.

--- media/admin/js/ka/FieldTypes/View.js
+++ media/admin/js/ka/FieldTypes/View.js
@@ -10,17 +10,40 @@ ka.FieldTypes.View = new Class({
 
     },
 
+    module: '',
+    path: '',
+
     initialize: function(pFieldInstance, pOptions){
 
         pOptions.object = 'Core\\View';
-        pOptions.objectBranch = pOptions.directory ? pOptions.directory : true;
 
+        if (pOptions.directory == '')
+            throw 'Option `directory` is empty in ka.Field `view`.';
+
+        if (pOptions.directory.substr(0,1) == '/')
+            pOptions.directory = pOptions.directory.substr(1);
+
+        if (pOptions.directory.substr(pOptions.directory.length-1, 1) != '/')
+            pOptions.directory += '/';
+
+        var sIdx    = pOptions.directory.indexOf('/');
+        this.module = pOptions.directory.substr(0, sIdx);
+        this.path   = pOptions.directory.substr(sIdx+1);
+
+        pOptions.objectBranch = pOptions.directory ? pOptions.directory : true;
         this.parent(pFieldInstance, pOptions);
     },
 
-
     getValue: function(){
         var value = this.parent();
-        return value.substr(this.options.directory.length);
+        value = value.path;
+        return value.substr((this.module+'/'+this.path).length);
+    },
+
+    setValue: function(pValue){
+        if (typeOf(pValue) == 'string'){
+            pValue = (this.module+'/'+this.path)+pValue;
+        }
+        this.parent({path: pValue});
     }
 });
