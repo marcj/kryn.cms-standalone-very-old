8fea840042b72f44504912e9af318ee90ef702d3
MArcJ <marc@kryn.org>
2013-02-16 03:31:57 +0100

    Added possibility to define the js script tag position through smarty's {addJs}

--- core/Template/SmartyPlugins/function.addJs.php
+++ core/Template/SmartyPlugins/function.addJs.php
@@ -1,4 +1,4 @@
 <?php
 function smarty_function_addJs($params, &$smarty){
-    Core\Kryn::getResponse()->addJsFile( $params['file'] );
+    Core\Kryn::getResponse()->addJsFile( $params['file'], $params['position'] ?: 'top' );
 }
