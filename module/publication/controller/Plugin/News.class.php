d25899e6173514e2a9500d25b1adf0402e0a7641
MArcJ <marc@kryn.org>
2013-02-17 02:00:05 +0100

    Adjusted the News::listing plugin

--- module/publication/controller/Plugin/News.class.php
+++ module/publication/controller/Plugin/News.class.php
@@ -2,8 +2,9 @@
 
 namespace Publication\Plugin;
 
-use Core\Controller;
 use Publication\NewsQuery;
+use Core\Controller;
+use Core\Kryn;
 
 class News extends Controller {
 
@@ -20,14 +21,31 @@ class News extends Controller {
         $cacheKey = 'publication/news/list/'.$page;
         $view     = 'publication/news/list/'.$options['template'];
 
-        return $this->renderCached($cacheKey, $view, function() use ($page, $options){
+        return $this->renderFullCached($cacheKey, $view, function() use ($page, $options){
 
-            $items = NewsQuery::create()
-                ->paginate($page, $options['itemsPerPage'] ?: 10)
+            $paginate = NewsQuery::create()->paginate($page, $options['itemsPerPage'] ?: 10);
+
+
+            if ($page > $paginate->getLastPage()){
+                return null;
+                //because if we return here NULL the `renderFullCached` function returns NULL as well,
+                //and this means that we say to the HTTPKernel 'we (this route) are not responsible' for the current request.
+                //the HTTPKernel removes this current route then from the RouteCollection
+                //and starts a new SUB_REQUEST. Maybe another route is here that handles this request.
+            }
+
+            $items    = $paginate
                 ->getResults()
                 ->toArray(null, null, \BasePeer::TYPE_STUDLYPHPNAME);
 
-            return array('items' => $items);
+            foreach ($items as &$item){
+                $item['url'] = ($options['detailPage'] ? Kryn::getPageUrl($options['detailPage']) : '') . $item['uri'];
+            }
+
+            return array(
+                'items'     => $items,
+                'options'   => $options
+            );
 
         });
     }
