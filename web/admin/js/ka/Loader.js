09c41b0d332398823f2189f1d63fcabfc091a72b
marcj <marc@kryn.org>
2013-03-03 18:12:27 +0100

    Adjusted the ka.Loader class. Added png based image loader

--- web/admin/js/ka/Loader.js
+++ web/admin/js/ka/Loader.js
@@ -1,21 +1,39 @@
 ka.Loader = new Class({
-    initialize: function (pTooltip, pTransBg) {
 
+    Binds: ['initAnimation', 'startAnimation', 'continueAnimation'],
+    Implements: [Events, Options],
 
-        this.src = _path + 'admin/images/';
-        if (pTooltip == true) {
-            this.src += 'ka-tooltip-loading.gif';
-        } else {
-            this.src += 'loading.gif';
+    options: {
+        loader: 'admin/images/loader-sprites-18.png',
+        spriteWidth: 18,
+        absolute: false,
+        big: false,
+        overlay: false
+    },
+
+    src: '',
+
+    index: 0,
+    posX: 0,
+
+    initialize: function (pContainer, pOptions) {
+
+        this.setOptions(pOptions);
+
+        if (this.options.big){
+            this.options.loader = 'admin/images/loader-sprites-64.png';
+            this.options.spriteWidth = 64;
         }
 
-        this.main = new Element('div', {
-            'class': 'ka-loader-main'
-        });
+        this.src = _path + this.options.loader;
 
-        if (!pTransBg) {
-            this.main.setStyle('background-color', '#eee');
-        } else {
+        if (this.options.absolute){
+            this.main = new Element('div', {
+                'class': 'ka-loader-main-absolute'
+            });
+        }
+
+        if (this.options.overlay) {
             this.transBg = new Element('div', {
                 style: 'position: absolute; left: 0px; right: 0px; top: 0px; bottom: 0px; background-color: #eee;',
                 styles: {
@@ -29,20 +47,82 @@ ka.Loader = new Class({
             styles: {
                 width: '100%', height: '100%'
             }
-        }).inject(this.main);
+        }).inject(this.main ? this.main : pContainer);
+
         var tr = new Element('tr').inject(this.loadingTable);
-        var td = new Element('td', {align: 'center', valign: 'center', width: '100%', height: '100%'}).inject(tr);
-        this.img = new Element('img', {src: this.src}).inject(td);
-        this.td = td;
+        this.td = new Element('td', {
+            align: 'center',
+            valign: 'center',
+            width: '100%',
+            height: '100%',
+            styles: {
+                'vertical-align': 'middle',
+                'line-height': 1
+            }
+        }).inject(tr);
+
+        this.loader = new Element('div', {
+            style: 'display: inline-block;'
+        }).inject(this.td);
+
+        this.assetImage = Asset.image(this.src, {
+            onLoad: this.initAnimation
+        });
+
+        if (typeOf(pContainer) == 'element') {
+            this.inject(pContainer);
+        }
+    },
+
+    initAnimation: function(){
+        this.loader.setStyles({
+            width: this.options.spriteWidth,
+            height: this.assetImage.height
+        });
+
+        this.loader.setStyle('background-image', 'url('+this.src+')');
+
+        this.fps   = Math.round(100/9);
+        this.delay =  1 / this.fps;
+
+        this.startAnimation();
+    },
+
+    startAnimation: function(){
+        this.posX  = 0;
+        this.index = 0;
+        this.continueAnimation();
+    },
+
+    continueAnimation: function(){
+
+        if (this.toElement().getStyle('display') == 'none') return;
+
+        this.posX  += this.options.spriteWidth;
+        this.index += 1;
+
+        if (this.index >= 8) {
+            this.posX  = 0;
+            this.index = 0;
+        }
+
+        this.loader.setStyle('background-position', (-this.posX)+'px 0');
+
+        this.lastAnimationTimer = this.continueAnimation.delay(this.delay*1000);
+
     },
 
     setStyle: function (p, p2) {
-        this.main.setStyle(p, p2);
+        this.toElement().setStyle(p, p2);
+    },
+
+    toElement: function(){
+        return this.main ? this.main : this.loadingTable;
     },
 
     destroy: function () {
-        if (this.main) {
-            this.main.destroy();
+        if (this.toElement()) {
+            this.toElement().destroy();
         }
         if (this.transBg) {
             this.transBg.destroy();
@@ -50,7 +130,7 @@ ka.Loader = new Class({
     },
 
     inject: function (pTarget, pWhere) {
-        this.main.inject(pTarget, pWhere);
+        this.toElement().inject(pTarget, pWhere);
 
 
         if (this.transBg) {
@@ -61,17 +141,23 @@ ka.Loader = new Class({
     },
 
     show: function () {
-        this.main.setStyle('display', 'block');
+        this.toElement().setStyle('display', this.toElement().get('tag') == 'table' ? 'table' : 'block');
 
         if (this.transBg) {
             this.transBg.setStyle('display', 'block');
         }
 
+        this.startAnimation();
         return this;
     },
 
     hide: function () {
-        this.main.setStyle('display', 'none');
+
+        this.toElement().setStyle('display', 'none');
+
+        if (this.lastAnimationTimer) {
+            clearTimeout(this.lastAnimationTimer);
+        }
 
         if (this.transBg) {
             this.transBg.setStyle('display', 'none');
