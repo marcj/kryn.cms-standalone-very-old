a2daeb924f22f4656e95e49b4b1bc443bd6cac92
marcj <marc@kryn.org>
2013-07-28 20:52:14 +0200

    typo

--- tests/Tests/RestTestCase.php
+++ tests/Tests/RestTestCase.php
@@ -30,7 +30,7 @@ class RestTestCase extends TestCaseWithCore
 
         if ($failOnError && (!is_array($data) || $data['error'])) {
             $this->fail(
-                "path $path, method:$method: (status code: {$info['http_code']}\n".
+                "path $path, method: $method: (status code: {$info['http_code']}\n".
                 $info['content']
             );
         }
