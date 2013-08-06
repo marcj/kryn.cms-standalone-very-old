e62cb5e4f33a7dd91130b22d16ee96a0dfb6914a
marcj <marc@kryn.org>
2013-08-06 15:38:06 +0200

    Added BundleCache class

---
+++ src/Core/Config/BundleCache.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Core\Config;
+
+use Core\Kryn;
+
+class BundleCache extends Model
+{
+    protected $rootName = 'cache';
+    protected $attributes = ['method'];
+    protected $nodeValueVar = 'key';
+
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * @param string $key
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $method
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+}
